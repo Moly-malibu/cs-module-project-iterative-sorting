d19514c085d278f84ba3e33fb22d3b2ef4f02a89
Monica Bustamante <lilianalambda@gmail.com>
2020-07-20 13:45:49 -0700

    Iterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,23 +4,47 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
 
-
-        # TO-DO: swap
-        # Your code here
-
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i] # TO-DO: swap
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    did_swap = True
+    iterations = 0
+    while did_swap:
+        did_swap = False
+        for i in range(len(arr) - iterations - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                did_swap = True
+        iterations +=1
     return arr
+
+
+#Example 2 Rutledge
+# def insertion_sort(arr):
+#     # loops through the array starting at second element
+#     for i in range(1, len(arr)):
+#         # capture element to sort
+#         cur = arr[i]
+#         # capture the index to mutate
+#         j = i
+#         # comparison loop
+#         while j > 0 and arr[j-1] > cur:
+#             # moves greater item to right
+#             arr[j] = arr[j-1]
+#             # decrements comparison loop
+#             j -= 1
+#             # adds item to sort back into the array
+#         arr[j] = cur
+#         # return the finished array
+#     return arr
+
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +63,44 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+#Maximun
 def counting_sort(arr, maximum=None):
-    # Your code here
+    i = 0
+    j = n - 1
+    while(i < j): 
+        min = arr[i] 
+        min_i = i 
+        max = arr[i] 
+        max_i = i 
+        for T in range(i, j + 1, 1): 
+            if (arr[T] > max): 
+                max = arr[T] 
+                max_i = T 
+            elif (arr[T] < min): 
+                min = arr[T] 
+                min_i = T  
+        time = arr[i] 
+        arr[i] = arr[min_i] 
+        arr[min_i] = time 
+        if (arr[min_i] == max): 
+            time = arr[j] 
+            arr[j] = arr[min_i] 
+            arr[min_i] = time 
+        else: 
+            time = arr[j] 
+            arr[j] = arr[max_i] 
+            arr[max_i] = time 
+  
+        i += 1
+        j -= 1
+  
+    print("Sorted array:", end = " ") 
+    for i in range(n): 
+        print(arr[i], end = " ")  
+
+if __name__== '__main__': 
+    arr = [53, 76, 41, 7, 33, 86, 1] 
+    n = len(arr) 
+    counting_sort(arr, n) 
 
 
-    return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,22 @@
+#linear search.
+
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
-
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    low = 0
+    hight = len(arr)-1
+    mid = 0
+    while hight >= low:
+        mid = (hight + low) // 2
+        if arr[mid] > target:
+            hight = mid -1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            return mid
     return -1  # not found
